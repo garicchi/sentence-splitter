1dd7e2218c0344770aec9820f907b35a41dc6ae1
garicchi <xgaricchi@gmail.com>
2016-08-17 21:01:02 +0900

    initialize

---
+++ spec/sentence-splitter-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import SentenceSplitterView from '../lib/sentence-splitter-view';
+
+describe('SentenceSplitterView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
